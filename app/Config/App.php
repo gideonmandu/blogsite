b17f869ccf7e16cb1ec864c7c552dc3156fb9bea
gideonmandu <gideonmandu@gmail.com>
2021-11-11 21:20:10 +0300

    configure application routes

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/blogsite/public/';
+    public $baseURL = 'http://localhost/';
 
     /**
      * --------------------------------------------------------------------------
