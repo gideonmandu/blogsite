8aa980fe3cfb1bc06c2036e30616b830cd2e643e
gideonmandu <gideonmandu@gmail.com>
2021-11-11 21:19:07 +0300

    added home and about pages

---
+++ app/Views/pages/about.php
@@ -0,0 +1,8 @@
+<section>
+    <div class="container-sm py-5">
+        <h1 class="text-dark fw-bold">About us</h1>
+        <p class="text-canter col-8">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis quos numquam perferendis distinctio vero in tempora veniam, tempore, magni quas sit ipsam commodi. Iste sunt ex nemo molestias velit error.</p>
+        <h1 class="text-dark fw-bold">learn More</h1>
+        <p class="text-center col-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti libero ab perspiciatis maiores tenetur, consequatur facere ratione eos error vel est sapiente quos, cupiditate quae nesciunt voluptatem debitis, porro atque.</p>
+    </div>
+</section>
---
+++ app/Views/pages/home.php
@@ -0,0 +1,9 @@
+<section>
+    <div class="container-sm">
+        <h1 class="display-4 fw-bolder">Dummy Blog site</h1>
+        <p>Demo Code Igniter Project</p>
+        <hr>
+        <p class="text-secondary">Trying out php 8</p>
+        <p><a href="#" class="btn btn-primary btn-lg" role="button">Learn More</a></p>
+    </div>
+</section>
