d40b6b8b0ae0534b2efa4c743f6ee74397dcd268
gideonmandu <gideonmandu@gmail.com>
2021-11-13 01:34:51 +0300

    page view styling to keep footer at bottom

--- app/Views/pages/about.php
+++ app/Views/pages/about.php
@@ -1,8 +1,8 @@
 <section>
     <div class="container-sm py-5">
-        <h1 class="text-dark fw-bold">About us</h1>
-        <p class="text-canter col-8">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis quos numquam perferendis distinctio vero in tempora veniam, tempore, magni quas sit ipsam commodi. Iste sunt ex nemo molestias velit error.</p>
-        <h1 class="text-dark fw-bold">learn More</h1>
-        <p class="text-center col-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti libero ab perspiciatis maiores tenetur, consequatur facere ratione eos error vel est sapiente quos, cupiditate quae nesciunt voluptatem debitis, porro atque.</p>
+        <p class="h2 text-dark fw-bold">About us</p>
+        <p class="text-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis quos numquam perferendis distinctio vero in tempora veniam, tempore, magni quas sit ipsam commodi. Iste sunt ex nemo molestias velit error.</p>
+        <p class="h2 text-dark fw-bold">learn More</p>
+        <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti libero ab perspiciatis maiores tenetur, consequatur facere ratione eos error vel est sapiente quos, cupiditate quae nesciunt voluptatem debitis, porro atque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam praesentium molestias iste laborum voluptatibus optio quaerat cum, fugit inventore maiores labore dolorum facilis possimus saepe corporis accusamus nesciunt aliquid pariatur!</p>
     </div>
 </section>
