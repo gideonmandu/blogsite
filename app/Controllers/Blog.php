734590ef55188353c7334a6781054049e7de2928
gideonmandu <gideonmandu@gmail.com>
2021-11-13 00:31:04 +0300

    added update and delete functions
    added crud methods to allow updating and deleting of posts

--- app/Controllers/Blog.php
+++ app/Controllers/Blog.php
@@ -45,4 +45,47 @@ class Blog extends BaseController
             return redirect()->to('/');
         }
     }
+
+    function update($id)
+    {
+        helper('form');
+        $model = new BlogModel();
+        $data['post'] = $model->getPost($id);
+
+        if (!$this->validate(
+            // validate form input
+            [
+                'title' => 'required|min_length[3]|max_length[255]',
+                'body' => 'required'
+            ]
+        )) {
+            echo view('templates/header', $data);
+            echo view('blog/update');
+            echo view('templates/footer');
+        } else {
+            // saving blog post
+            $model->save(
+                [
+                    'id' => $id,
+                    'title' => $this->request->getVar('title'),
+                    'body' => $this->request->getVar('body'),
+                    'slug' => url_title($this->request->getVar('title')),
+                ]
+            );
+            $session = \Config\Services::session();
+            $session->setFlashdata('success', 'Post with id: ' . $id . ' was Updated');
+            return redirect()->to('/blog/' . url_title($this->request->getVar('title')));
+        }
+    }
+
+    function delete($id)
+    {
+        $model = new BlogModel();
+
+        $data['post'] = $model->deletePost($id);
+        $session = \Config\Services::session();
+        $session->setFlashdata('success', 'Post with id: ' . $id . ' was Deleted');
+
+        return redirect()->to('/');
+    }
 }
