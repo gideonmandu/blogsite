f06a0f01ddec12161c57cd2c16d6e568e70b47aa
gideonmandu <gideonmandu@gmail.com>
2021-11-13 00:36:12 +0300

    added update post page

---
+++ app/Views/blog/update.php
@@ -0,0 +1,19 @@
+<div class="container-sm pt-3">
+    <h1>Update post</h1>
+    <?php if ($_POST) : ?>
+        <?= \Config\Services::validation()->listErrors(); ?>
+    <?php endif; ?>
+    <form class="mx-auto" action="/blog/update/<?= $post['id'] ?>" method="POST">
+        <div class="form-group">
+            <label for="title" class="fw-bold form-label">Title:</label>
+            <input type="text" name="title" class="form-control" id="title" value="<?= $post['title'] ?>">
+        </div>
+        <div class="form-group">
+            <label for="body" class="fw-bold form-label">Body:</label>
+            <textarea class="form-control" name="body" id="body" cols="30" rows="10"><?= $post['body'] ?></textarea>
+        </div>
+        <div class="form-group py-3">
+            <button type="submit" class="btn btn-primary mb-3">Update</button>
+        </div>
+    </form>
+</div>
