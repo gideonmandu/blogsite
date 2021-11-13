d1605e5c5af9770b039257b24009f3a17627fd8c
gideonmandu <gideonmandu@gmail.com>
2021-11-13 18:03:23 +0300

    added local bootstrap css & search button

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="<?php echo base_url("bootstrap-5.1.3/css/bootstrap.min.css"); ?>">
     <title>Blog site</title>
     <style>
         html {
@@ -45,11 +46,18 @@
                         <li class="nav-item">
                             <a class="nav-link" href="/about">About</a>
                         </li>
+                        <li class="nav-item">
+                            <a href="/blog/create" class="btn btn-outline-success" type="submit">Create Post</a>
+                        </li>
                     </ul>
-                    <div class="d-flex">
-                        <a href="/blog/create" class="btn btn-outline-success" type="submit">Create Post</a>
-                    </div>
+                    <form class="d-flex" method="POST" action="/blog/search">
+                        <input type="search" placeholder="Search Blog By Title" aria-label="Search" name="title" class="form-control me-2" id="title">
+                        <button class="btn btn-outline-warning" type="submit">Search</button>
+                    </form>
                 </div>
             </div>
         </nav>
+        <?php if ($_POST) : ?>
+            <?= \Config\Services::validation()->listErrors(); ?>
+        <?php endif; ?>
         <main>
