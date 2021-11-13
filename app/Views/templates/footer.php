0cd19df895919784d71840922d2f78194797c55d
gideonmandu <gideonmandu@gmail.com>
2021-11-13 17:59:54 +0300

    added local bootstrap js

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,4 +1,5 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> -->
+<script src="<?php echo base_url('bootstrap-5.1.3/js/bootstrap.bundle.min.js') ?>"></script>
 </main>
 </div>
 <footer class="container-fluid bg-light">
